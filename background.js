5d7d68f6f665ca56d14f3775b632fbf5edf67d5e
Aditya Manoharan <adityavazhipokkil@gmail.com>
2020-09-28 15:36:46 -0400

    Remove unused link match

--- background.js
+++ background.js
@@ -2,13 +2,13 @@ chrome.webRequest.onBeforeRequest.addListener(
   ({ url }) => {
     const [fullMatch, type, identifier] =
       url.match(
-        /spotify\.com\/(track|album|artist|playlist|concert|episode|show)\/([^\&\#\/\?]+)/i
+        /open\.spotify\.com\/(track|album|artist|playlist|concert|episode|show)\/([^\&\#\/\?]+)/i
       ) || [];
 
     return fullMatch ? { redirectUrl: `spotify:${type}:${identifier}` } : null;
   },
   {
-    urls: ['https://open.spotify.com/*', 'https://play.spotify.com/*'],
+    urls: ['https://open.spotify.com/*'],
     types: ['xmlhttprequest'],
   },
   ['blocking']
